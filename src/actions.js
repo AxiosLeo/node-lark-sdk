49efa72b49252db86a8c11096c8e3f87da60a362
axiosleo <axiosleo@foxmail.com>
2022-11-19 13:59:01 +0800

    refactor: actions

--- src/actions.js
+++ src/actions.js
@@ -1,42 +1,46 @@
 'use strict';
 
-class Action {
+const Model = require('./model');
+
+class Action extends Model {
   constructor(tag) {
-    this.action = {
+    super({
       tag
-    };
+    });
   }
 }
 
 class ButtonAction extends Action {
   constructor({ text, type = 'default', url, multi_url, value, confirm }) {
-    super('button');
-    this.action.text = {
-      tag: 'plain_text',
-      content: text
-    };
-    this.action.type = type;
-    Object.assign(this.action, {
+    super({
+      tag: 'button',
+      text: {
+        tag: 'plain_text',
+        content: text
+      },
+      type,
       url, multi_url, value, confirm
     });
   }
 }
 
 class SelectOptionsAction extends Action {
-  constructor({ placeholder, options, initial_option, confirm, value }) {
-    super('select_static');
-    this.action.placeholder = {
-      tag: 'plain_text',
-      content: placeholder
-    };
-    this.action.options = options;
-    Object.assign(this.action, {
-      initial_option, confirm
+  constructor({ placeholder, options, initial_option, confirm }) {
+    super({
+      tag: 'select_static',
+      placeholder: {
+        tag: 'plain_text',
+        content: placeholder
+      },
+      options,
+      initial_option,
+      confirm
     });
   }
 }
 
 module.exports = {
+  Action,
   ButtonAction,
   SelectOptionsAction
 };
