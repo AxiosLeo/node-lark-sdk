4f45973add8f9c5cce1305bdde2d7db94a2c67c9
axiosleo <axiosleo@foxmail.com>
2022-11-22 23:16:19 +0800

    refactor: elements

--- src/elements.js
+++ src/elements.js
@@ -1,31 +1,45 @@
 'use strict';
 
+// eslint-disable-next-line no-unused-vars
+const { Action } = require('./actions');
+// eslint-disable-next-line no-unused-vars
+const { Field } = require('./fields');
 const { validate } = require('./helper');
+const Model = require('./model');
 
-class Element {
-  constructor(tag) {
-    this.element = {
-      tag,
-    };
+class Element extends Model {
+  constructor(obj = {}) {
+    if (!obj.tag) {
+      throw new Error('Element tag is required');
+    }
+    super(obj);
   }
 }
 
 class PlainTextElement extends Element {
-  constructor(content, fields = [], extra = {}) {
-    super('div');
-    this.element.text = {
-      tag: 'plain_text',
-      content,
-      ...extra
-    };
-    this.element.fields = fields;
+  /**
+   * @param {string} content 
+   * @param {Field[]} fields
+   */
+  constructor(content, fields = [], lines = undefined) {
+    super({
+      tag: 'div',
+      text: {
+        tag: 'plain_text',
+        content,
+        lines,
+      },
+      fields,
+    });
   }
 }
 
 class MarkdownElement extends Element {
   constructor(content, href) {
-    super('markdown');
-    this.element.content = content;
+    super({
+      tag: 'markdown',
+      content
+    });
     if (href) {
       const errors = validate(href, {
         'url': 'required|string',
@@ -36,7 +50,7 @@ class MarkdownElement extends Element {
       if (errors.length > 0) {
         throw new Error('Invalid href');
       }
-      this.element.href = {
+      this.href = {
         urlVal: href
       };
     }
@@ -45,28 +59,41 @@ class MarkdownElement extends Element {
 
 class HrElement extends Element {
   constructor() {
-    super('hr');
+    super({
+      tag: 'hr'
+    });
   }
 }
 
 class ImageElement extends Element {
-  constructor({ title = '', img_key, mode = 'crop_center', preview = false, alt = '', custom_width, compact_width }) {
-    super('img');
-    this.element.title = {
-      tag: 'plain_text',
-      content: title
-    };
-    this.element.img_key = img_key;
-    this.element.mode = mode === 'fit_horizontal' ? mode : 'crop_center';
-    this.element.preview = preview;
+  constructor({
+    title = '',
+    img_key,
+    mode = 'crop_center',
+    preview = false,
+    alt = '',
+    custom_width,
+    compact_width
+  }) {
+    super({
+      tag: 'img',
+      title: {
+        tag: 'plain_text',
+        content: title
+      },
+      img_key,
+      mode: mode === 'fit_horizontal' ? mode : 'crop_center',
+      preview
+    });
+
     if (custom_width) {
-      this.element.custom_width = custom_width;
+      this.custom_width = custom_width;
     }
     if (compact_width) {
-      this.element.compact_width = compact_width;
+      this.compact_width = compact_width;
     }
     if (alt) {
-      this.element.alt = {
+      this.alt = {
         tag: 'plain_text',
         content: alt
       };
@@ -86,13 +113,19 @@ class NoteElement extends Element {
 }
 
 class ActionElement extends Element {
-  constructor(actions) {
-    super('action');
-    this.element.actions = actions;
+  /**
+   * @param {Action[]} actions 
+   */
+  constructor(actions = []) {
+    super({
+      tag: 'action',
+      actions
+    });
   }
 }
 
 module.exports = {
+  Element,
   PlainTextElement,
   MarkdownElement,
   HrElement,
